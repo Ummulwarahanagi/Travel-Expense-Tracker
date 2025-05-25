349463240d5f9da12abe9e5d31a89311631cb317
Ummulwarahanagi <154959593+Ummulwarahanagi@users.noreply.github.com>
2025-05-25 13:04:20 +0530

    Update script.py

--- script.py
+++ script.py
@@ -28,7 +28,7 @@ if not username:
 st.title(f"Welcome {username}")
 # --- Logout Button ---
 if st.sidebar.button("🚪 Logout"):
-    st.experimental_set_query_params()  # Clears all query params
+    st.query_params  # Clears all query params
     st.rerun()
 
 

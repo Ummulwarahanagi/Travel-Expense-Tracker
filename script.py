e13200233102f50f757e344a7794b79d53f57805
Ummulwarahanagi <154959593+Ummulwarahanagi@users.noreply.github.com>
2025-05-25 10:55:47 +0530

    Update script.py

--- script.py
+++ script.py
@@ -13,6 +13,16 @@ from google_sheets_utils import (
 st.set_page_config(page_title="Travel Expense Tracker", layout="wide")
 gsheet = connect_sheet()
 
+# Get the logged-in user from the URL
+params = st.query_params
+username = params.get("user", ["Guest"])[0]
+
+# Load the data
+df = pd.read_csv("gsheet")
+
+# Filter data for logged-in user
+user_data = df[df['username'] == username]
+
 #Budget Input Sidebar
 st.sidebar.header("💰Set A Budget")
 curr_budget = get_budget(gsheet)
@@ -36,10 +46,9 @@ with st.sidebar.form("add_expense"):
 
 #Load Expense Data
 df = load_ex_gsheet(gsheet)
-params = st.query_params
-user = params.get("user", ["Guest"])[0]
+st.title(f"Welcome, {username} 👋")
+st.dataframe(user_data)
 
-st.title(f"Welcome {user} 👋")
 st.markdown("📊 Budget Overview")
 
 if not df.empty:
